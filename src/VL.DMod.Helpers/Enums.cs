4fde1230a0f0d69a78f64b36f5da99f4e0c02a12
cnisidis <makeithappen@nisidis.com>
2021-11-02 12:22:02 +0200

    re-started | old folder wont be uploaded (previous iteration)

---
+++ src/VL.DMod.Helpers/Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VL.DMod.Helpers
+{
+    public enum DModuleType
+    {
+        R1,
+        R2,
+        R3,
+        R4,
+        C1,
+        C2,
+        C3,
+        C4
+    }
+
+    public enum DSegmentPlacement
+    {
+        None,//δαπέδου
+        W //επιτοίχειο
+
+    }
+        
+
+
+    public enum DSegmentType
+    {
+        R,  //only R
+        RE, //R extended
+        C, //
+        U01 //πόδι για ντουλάπι
+    }
+}
